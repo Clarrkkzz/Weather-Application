9dfe43e243909f0d502f05fde0125435aa6b084f
clark12123 <66197232+clark12123@users.noreply.github.com>
2026-09-13 23:29:20 +0400

    test2

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import httpx
+import redis 
+from redisapp import fifofull
 
 load_dotenv()
 
 app = FastAPI()
+
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 
 @app.get("/")
@@ -16,9 +20,19 @@ async def root():
 async def get_weather(location: str, start_date: str, end_date: str):
     try:
         async with httpx.AsyncClient() as client:
+                cache_key = f"{location}_{start_date}_{end_date}"
+                if redis_client.exists(cache_key):
+                    cached_data = redis_client.get(cache_key)
+                    return {"data": cached_data, "source": "cache"}
+
                 response = await client.get(f"{base_url}/{location}/{start_date}/{end_date}?unitGroup=metric&key={API_KEY}&contentType=json")
+                #when didn't get a response from cache 
                 if response.status_code == 200:
-                    return response.json()
+                    data = response.json()
+                    redis_client.set(cache_key, str(data))  # Cache for 1 hour
+                    redis_client.rpush("fifo_queue", cache_key)  # Add to FIFO queue
+                    await fifofull.noofelement()  # Check if we need to clear the cache
+                    return data
                 else:
                     return {"error": "Unable to fetch weather data", "status_code": response.status_code}
     except Exception as e:
