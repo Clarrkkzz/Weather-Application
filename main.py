84f96957e2d8a33c0bdf6f6a745ae74cdac121f0
clark12123 <66197232+clark12123@users.noreply.github.com>
2026-09-13 20:35:03 +0400

    Added caching system

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import httpx
+import redis 
 
 load_dotenv()
 
 app = FastAPI()
+
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 
 @app.get("/")
@@ -16,11 +19,18 @@ async def root():
 async def get_weather(location: str, start_date: str, end_date: str):
     try:
         async with httpx.AsyncClient() as client:
-                response = await client.get(f"{base_url}/{location}/{start_date}/{end_date}?unitGroup=metric&key={API_KEY}&contentType=json")
-                if response.status_code == 200:
-                    return response.json()
+                cache_key = f"{location}_{start_date}_{end_date}"
+                if redis_client.exists(cache_key):
+                    cached_data = redis_client.get(cache_key)
+                    return {"data": cached_data, "source": "cache"}
                 else:
-                    return {"error": "Unable to fetch weather data", "status_code": response.status_code}
+                    response = await client.get(f"{base_url}/{location}/{start_date}/{end_date}?unitGroup=metric&key={API_KEY}&contentType=json")
+                    if response.status_code == 200:
+                        data = response.json()
+                        redis_client.set(cache_key, str(data))  # Cache for 1 hour
+                        return data
+                    else:
+                        return {"error": "Unable to fetch weather data", "status_code": response.status_code}
     except Exception as e:
         return {"error": str(e)}
     
