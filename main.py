1286357dd168bfea0f60b966a80b583bd8ec935e
clark12123 <66197232+clark12123@users.noreply.github.com>
2026-09-13 18:52:08 +0400

    sends json back to user successfuly

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import os
 from fastapi import FastAPI
-import requests
 from dotenv import load_dotenv
+import httpx
 
 load_dotenv()
 
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Weather API"}
+
+@app.get("/weather/{location}/{start_date}/{end_date}")
+async def get_weather(location: str, start_date: str, end_date: str):
+    try:
+        async with httpx.AsyncClient() as client:
+                response = await client.get(f"{base_url}/{location}/{start_date}/{end_date}?unitGroup=metric&key={API_KEY}&contentType=json")
+                if response.status_code == 200:
+                    return response.json()
+                else:
+                    return {"error": "Unable to fetch weather data", "status_code": response.status_code}
+    except Exception as e:
+        return {"error": str(e)}
+    
+        
+    
+
 API_KEY = os.getenv("VISUAL_CROSSING_API_KEY")
 base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
-
-response = requests.get(f"{base_url}/New York?unitGroup=metric&key={API_KEY}&contentType=json")
-
-print(response.json())
